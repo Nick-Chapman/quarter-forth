6bb7e90570fe9758cd4e9c45aabbffda698e9b13
Nick Chapman <nic@amadido.co.uk>
2023-08-19 15:16:56 +0100

    more predefined + regression

--- src/predefined.f
+++ src/predefined.f
@@ -1,10 +1,14 @@
+
 : false     0 ;
 : true      65535 ;
-: not       true swap if drop false then ;
-: >         swap < ;
-: square    dup * ;
-: min       over over > if swap then drop ;
-: expect    over = not if hey . then drop ;
+: invert    true swap if drop false then ;
 
+: >         swap < ;
+: <>        = invert ;
+
+: square    dup * ;
 : fact      dup 1 > if dup 1 - fact * then ;
 : fib       dup 1 > if dup 1 - fib swap 2 - fib + then ;
+
+: min       over over > if swap then drop ;
+: max       over over < if swap then drop ;
--- src/regression.f
+++ src/regression.f
@@ -1,16 +1,29 @@
+: expect    over = invert if hey . then drop ;
 
-false not true expect
-true not false expect
+3 square square 81 expect
+6 fact 720 expect
+10 fib 55 expect
 
+false invert true expect
+true invert false expect
+
+3 3 = true expect
+3 4 = false expect
+3 3 <> false expect
+3 4 <> true expect
 3 4 < true expect
 4 3 < false expect
 4 3 > true expect
 3 4 > false expect
 
-3 square square 81 expect
 5 6 min 5 expect
 6 5 min 5 expect
+5 6 max 6 expect
+6 5 max 6 expect
 
-6 fact 720 expect
-10 fib 55 expect
-
+3 4 + 7 expect
+4 3 + 7 expect
+2 5 - 65533 expect
+5 2 - 3 expect
+3 4 * 12 expect
+4 3 * 12 expect
