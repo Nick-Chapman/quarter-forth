278a154d27ef687df55ba500ae5e6c6ba7e58993
Nick Chapman <nic@amadido.co.uk>
2023-09-11 21:59:03 +0100

    remove overly verbose commenting in interpreter

--- f/interpreter.f
+++ f/interpreter.f
@@ -1,37 +1,11 @@
-.." Loading interpeter ( " latest
+.." Loading interpeter" cr
 
 : [
-( debugging... ) ( [char] > emit )
-word ( string )
-
-( Is the word the special "]" marker? )
-dup s" ]" s= if ( string )
-
-( YES, exit the interpreter loop. )
-drop exit then
-
-( Is the word in the dictionary? )
-dup find dup if ( string xt )
-
-( YES, execute it, and loop... )
-swap drop ( xt ) execute tail [
-
-( NO, name is not in the dictionary... )
-then drop ( string )
-
-( Maybe it's a number... )
-number? if ( converted-number )
-
-( YES; leave the converted number on the stack, and loop... )
-tail [
-
-( NO, word is undefined, so message, skip and loop... )
-
-then ." ** Interpreter: '" type ." ' ?" cr
-crash-only-during-startup tail [
-
+word
+dup s" ]" s= if drop exit then
+dup find dup if swap drop execute tail [
+then drop number? if tail [
+then ." ** Interpreter: '" type ." ' ?" cr crash-only-during-startup tail [
 ; immediate
 
-( enter the interpreter ) [
-
-words-since char ) emit cr
+[
